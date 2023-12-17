ce53f4dc912748fe9fb07f1a03385a3f11a9de41
Lucas Malacrida Dias <lucasmd99@hotmail.com>
2023-12-16 22:36:55 -0300

    Conexão da Tela de Tickets e Pagamento c/ BackEnd

--- src/pages/Dashboard/Payment/Payments.jsx
+++ src/pages/Dashboard/Payment/Payments.jsx
@@ -1,19 +1,34 @@
-import CreditCard from "../../../components/CreditCard/CreditCad";
+import { useState } from "react";
+import CreditCard from "../../../components/CreditCard/CreditCard";
+import { number as cardNumberValidator } from 'card-validator';
 import PaymentConfirmedMessage from "../../../components/PaymentConfirmed/PaymentConfirmedMessage";
+import { postPayment } from "../../../services/paymentApi";
 
-export default function Payments({ ticket, accommodation, StyledTypography, StyledParagraph, Row, SummaryBox, ConfirmButton, paymentData, setPaymentData }) {
-    let ticketType = '';
-    let ticketPrice = 0;
-    if (paymentData.confirm) {
-        ticketType = paymentData.type;
-        ticketPrice = paymentData.price;
-    } else {
-        ticketType = (ticket.type === 'Online')? ticket.type : `${ticket.type} + ${accommodation.type}`;
-        ticketPrice = ticket.price + accommodation.price;
-    }
+export default function Payments({ userData, userTicket, setUserTicket, StyledTypography, StyledParagraph, Row, SummaryBox, ConfirmButton }) {
+    const [issuer, setIssuer] = useState('');
+    const [number, setNumber] = useState('');
+    const [name, setName] = useState('');
+    const [expirationDate, setExpirationDate] = useState('');
+    const [cvv, setCvv] = useState('');
+    const [focus, setFocus] = useState('');
+    const cardProps = { issuer, setIssuer, number, setNumber, name, setName, expirationDate, setExpirationDate, cvv, setCvv, focus, setFocus }
 
-    function confirmPayment() {
-        setPaymentData({ confirm: true, type: ticketType, price: ticketPrice });
+    async function confirmPayment() {
+        const cardNumberInfo = cardNumberValidator(number);
+        if (cardNumberInfo.card && cardNumberInfo.isValid) {
+            setIssuer(cardNumberInfo.card.type);
+
+            const paymentBody = {
+                ticketId: userTicket.id,
+                cardData: { issuer: cardNumberInfo.card.type, number, name, expirationDate, cvv }
+            };
+            try {
+                await postPayment(paymentBody, userData.token);
+                setUserTicket({...userTicket, status: "PAID"});
+            } catch (err) {
+                console.error('Error while confirming payment:', err);
+            }
+        }
     }
 
     return (
@@ -22,19 +37,20 @@ export default function Payments({ ticket, accommodation, StyledTypography, Styl
             <StyledParagraph>Ingresso escolhido</StyledParagraph>
             <Row>
                 <SummaryBox>
-                    <p>{ticketType}</p>
-                    <span>R$ {ticketPrice} </span>
+                    <p>{userTicket.TicketType.name}</p>
+                    <span>R$ {userTicket.TicketType.price / 100} </span>
                 </SummaryBox>
             </Row>
 
             <StyledParagraph>Pagamento</StyledParagraph>
-            {paymentData.confirm ?
+            {userTicket.status === "PAID" ?
                 <PaymentConfirmedMessage />
-                :
-                <>
-                    <CreditCard />
-                    <ConfirmButton onClick={() => confirmPayment()}>CONFIRMAR PAGAMENTO</ConfirmButton>
-                </>
+                : userTicket.status === "RESERVED" ?
+                    <>
+                        <CreditCard {...cardProps} />
+                        <ConfirmButton onClick={() => confirmPayment()}>CONFIRMAR PAGAMENTO</ConfirmButton>
+                    </>
+                    : <></>
             }
         </>
     )
--- src/pages/Dashboard/Payment/TicketSelection.jsx
+++ src/pages/Dashboard/Payment/TicketSelection.jsx
@@ -1,8 +1,26 @@
-export default function TicketSelection({ ticket, setTicket, accommodation, setAccommodation, StyledTypography, StyledParagraph, Row, BoxButton, ConfirmButton, setScreen }) {
+import { useState } from "react";
+import { postTicket } from "../../../services/ticketApi";
+
+export default function TicketSelection({ userData, ticketTypes, setUserTicket, StyledTypography, StyledParagraph, Row, BoxButton, ConfirmButton }) {
+    const [ticket, setTicket] = useState({ type: '', price: 0 });
+    const [accommodation, setAccommodation] = useState({ type: '', price: 0 });
     const totalPrice = (ticket.type === 'Presencial') ? (ticket.price + accommodation.price) : ticket.price;
 
-    function orderFinished() {
-        setScreen("Payments");
+    async function orderFinished() {
+        try {
+            let ticketType;
+            if ((ticket.type === 'Presencial') && (accommodation.type === 'Sem Hotel')) {
+                ticketType = { ticketTypeId: 1 };
+            } else if ((ticket.type === 'Presencial') && (accommodation.type === 'Com Hotel')) {
+                ticketType = { ticketTypeId: 2 };
+            } else if (ticket.type === 'Online') {
+                ticketType = { ticketTypeId: 3 };
+            }
+            const user_ticket = await postTicket(ticketType, userData.token);
+            setUserTicket(user_ticket);
+        } catch (err) {
+            console.error('Error while reserving ticket:', err);
+        }
     }
 
     return (
@@ -10,27 +28,27 @@ export default function TicketSelection({ ticket, setTicket, accommodation, setA
             <StyledTypography variant="h4">Ingresso e Pagamento</StyledTypography>
             <StyledParagraph>Primeiro, escolha sua modalidade de ingresso</StyledParagraph>
             <Row>
-                <BoxButton onClick={() => { setTicket({ type: 'Presencial', price: 250 }); ('Presencial') }} selected={ticket.type === 'Presencial'}>
+                <BoxButton onClick={() => { setTicket({ type: 'Presencial', price: ticketTypes[0].price / 100 }); ('Presencial') }} selected={ticket.type === 'Presencial'}>
                     <p>Presencial</p>
-                    <span>R$ 250</span>
+                    <span>R$ {ticketTypes[0].price / 100}</span>
                 </BoxButton>
-                <BoxButton onClick={() => { setTicket({ type: 'Online', price: 100 }); ('Online') }} selected={ticket.type === 'Online'}>
+                <BoxButton onClick={() => { setTicket({ type: 'Online', price: ticketTypes[2].price / 100 }); ('Online') }} selected={ticket.type === 'Online'}>
                     <p>Online</p>
-                    <span>R$ 100</span>
+                    <span>R$ {ticketTypes[2].price / 100}</span>
                 </BoxButton>
             </Row >
 
             {(ticket.type === 'Presencial') ?
                 <>
-                    <StyledParagraph StyledParagraph > Ótimo! Agora escolha sua modalidade de hospedagem</StyledParagraph>
+                    <StyledParagraph > Ótimo! Agora escolha sua modalidade de hospedagem</StyledParagraph>
                     <Row>
                         <BoxButton onClick={() => { setAccommodation({ type: 'Sem Hotel', price: 0 }); ('Presencial') }} selected={accommodation.type === 'Sem Hotel'}>
                             <p>Sem Hotel</p>
                             <span>+ R$ 0</span>
                         </BoxButton>
-                        <BoxButton onClick={() => { setAccommodation({ type: 'Com Hotel', price: 350 }); ('Presencial') }} selected={accommodation.type === 'Com Hotel'}>
+                        <BoxButton onClick={() => { setAccommodation({ type: 'Com Hotel', price: (ticketTypes[1].price / 100 - ticketTypes[0].price / 100) }); ('Presencial') }} selected={accommodation.type === 'Com Hotel'}>
                             <p>Com Hotel</p>
-                            <span>+ R$ 350</span>
+                            <span>+ R$ {ticketTypes[1].price / 100 - ticketTypes[0].price / 100}</span>
                         </BoxButton>
                     </Row>
                 </> : <></>
