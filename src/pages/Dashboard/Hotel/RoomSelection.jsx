5f7a0f01fcdefdea5ca89382e53c98f94a32a274
MariCMelo <marilia.correa.melo@gmail.com>
2023-12-18 15:14:52 -0300

    feat: ajuste lógica botão quartos

--- src/pages/Dashboard/Hotel/RoomSelection.jsx
+++ src/pages/Dashboard/Hotel/RoomSelection.jsx
@@ -4,17 +4,30 @@ import { toast } from "react-toastify";
 
 export default function RoomSelection({privateRoom, setPrivateRoom,sharedRoom, setSharedRoom, StyledParagraph, PrivateRoomButon, SharedRoomButon, PersonIcon, ConfirmButton}) {
     const [clickCount, setClickCount] = useState(0);
+    const [selectedRoom, setSelectedRoom] = useState(null);
     
-    const handlePrivateRoomClick = () => {
-        setPrivateRoom({ ...privateRoom, occupancy: 'Full' });
-    };
-    
-    const handleSharedRoomClick = () => {
-        setClickCount((prevCount) => Math.min(prevCount + 1, 3));
-        setSharedRoom({ ...sharedRoom, occupancy: 'Full' });
-    };
+const handleRoomClick = (roomType) => {
+  if (selectedRoom === roomType) {
+    setSelectedRoom(null);
 
-    
+    if (roomType === 'Private') {
+      setPrivateRoom({ ...privateRoom, occupancy: 'Empty' });
+    } else {
+      setSharedRoom({ ...sharedRoom, occupancy: sharedRoom.occupancy === 'Full' ? 'Empty' : 'Full' });
+    }
+  } else {
+    setSelectedRoom(roomType);
+
+    if (roomType === 'Private') {
+      setPrivateRoom({ ...privateRoom, occupancy: 'Full' });
+    } else {
+      if (sharedRoom.occupancy === 'Empty' && clickCount < 3) {
+        setClickCount((prevCount) => prevCount + 1);
+      }
+      setSharedRoom({ ...sharedRoom, occupancy: 'Full' });
+    }
+  }
+};
     const handleReservation = () => {
       if(privateRoom.occupancy === 'Empty' || sharedRoom.occupancy === 'Empty'){
         toast('Quarto reservado com sucesso!')
@@ -29,9 +42,9 @@ export default function RoomSelection({privateRoom, setPrivateRoom,sharedRoom, s
       <>
         <StyledParagraph>Ótima pedida! Agora escolha seu quarto:</StyledParagraph>
           <PrivateRoomButon 
-            onClick={handlePrivateRoomClick} 
-            selected={privateRoom.occupancy === 'Full'}
-            disabled={privateRoom.occupancy ==='Full'}
+             onClick={() => handleRoomClick('Private')}
+             selected={selectedRoom === 'Private'}
+             disabled={selectedRoom === 'Shared'}
             >101
               {privateRoom.occupancy === 'Full' ? (
                 <Person fontSize="27px" color="#8C8C8C" style={{ marginLeft: '10px' }} />
@@ -42,9 +55,9 @@ export default function RoomSelection({privateRoom, setPrivateRoom,sharedRoom, s
           </PrivateRoomButon>
 
         <SharedRoomButon 
-            onClick={handleSharedRoomClick} 
-            selected={sharedRoom.occupancy === 'Full'}
-            disabled={sharedRoom.occupancy === 'Full' || clickCount >= 3}
+            onClick={() => handleRoomClick('Shared')}
+            selected={selectedRoom === 'Shared'}
+            disabled={selectedRoom === 'Private' || clickCount >= 3}
             >
           10
           <PersonIcon>
@@ -54,25 +67,14 @@ export default function RoomSelection({privateRoom, setPrivateRoom,sharedRoom, s
               ) : (
                 <PersonOutline key={index} fontSize="27px" color="black" style={{ marginLeft: '10px' }} />
               )
-            )
-            )
+            )).reverse() 
             }
           </PersonIcon>
         </SharedRoomButon>
     
-    
         {(sharedRoom.occupancy === 'Full' || privateRoom.occupancy === 'Full') && (
         <ConfirmButton onClick={handleReservation}>RESERVAR QUARTO</ConfirmButton>
-        )}
-    
-          
-        {/* {(sharedRoom.occupancy === 'Full' || privateRoom.occupancy === 'Full') ?
-              <>
-                <ConfirmButton>RESERVAR QUARTO</ConfirmButton>
-              </> : <></>
-        } */}
-        
+        )}        
     </>
-    
   )
 }
